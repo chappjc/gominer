8115b61b1e73a94ae1278e15a42b16a2c93027f1
Jon Chappelow <jonathan.chappelow@gmail.com>
2016-11-27 11:40:39 -0800

    Modify for Intel OpenCL SDK

--- cl/cgo_flags.go
+++ cl/cgo_flags.go
@@ -1,9 +1,9 @@
 package cl
 
 /*
-#cgo CFLAGS: -I CL
+#cgo CFLAGS: -I /opt/intel/opencl-sdk/include/CL
 #cgo windows CFLAGS: -IC:/appsdk/include
-#cgo !darwin LDFLAGS: -lOpenCL
+#cgo !darwin LDFLAGS: -L/opt/intel/opencl-sdk/lib64 -lOpenCL
 #cgo darwin LDFLAGS: -framework OpenCL
 #cgo windows LDFLAGS: -LC:/appsdk/lib/x86_64
 */
